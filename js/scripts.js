b42d6189906fb7ab30f9169ed63378e30c66c9e2
Mark McConnell <amark.mcconnell@gmail.com>
2022-03-25 09:57:41 -0700

    add scripts logic/fx.

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
-$(document)
+$(document).ready(function() {
+  $("form#result").submit(function(event) {
+    const animal = parseInt($("#animal").val());
+    const color = parseInt($("#color").val())
+    const food = parseInt($("#food").val())
+    const gender = parseInt($("#gender").val())
+    const element = parseInt($("#element").val())
+    const output = $("#output");
+
+    if (animal === color && color === food && food === gender && gender === element) {
+      output.text("Ruby");
+    } else if (animal === 2 && color === animal)
+      output.text("C#")
+    event.preventDefault();
+  });
+});
