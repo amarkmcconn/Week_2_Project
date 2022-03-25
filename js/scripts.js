4991f4416d4eb0f8705d87402307ad5679b37558
Mark McConnell <amark.mcconnell@gmail.com>
2022-03-25 10:45:37 -0700

    add show/hide features.

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,20 @@
 $(document).ready(function() {
   $("form#result").submit(function(event) {
     const animal = parseInt($("#animal").val());
-    const color = parseInt($("#color").val())
-    const food = parseInt($("#food").val())
-    const gender = parseInt($("#gender").val())
-    const element = parseInt($("#element").val())
-    const output = $("#output");
+    const color = parseInt($("#color").val());
+    const food = parseInt($("#food").val());
+    const gender = parseInt($("#gender").val());
+    const element = parseInt($("#element").val());
 
     if (animal === color && color === food && food === gender && gender === element) {
-      output.text("Ruby");
-    } else if (animal === 2 && color === animal)
-      output.text("C#")
+      $("#ruby1").show();
+      $("#csharp").hide();
+    } else if (animal === 2 && color === animal) {
+      $("#csharp").show();
+      $("#ruby1").hide();
+    } else if (animal === 3 && color === animal)
+      $("jas1").show();
+
     event.preventDefault();
   });
 });
