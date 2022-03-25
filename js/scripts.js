ea83d1a6d525e1d024e346b7446e7920f274f684
Mark McConnell <amark.mcconnell@gmail.com>
2022-03-25 11:16:42 -0700

    add logic to scripts.

--- js/scripts.js
+++ js/scripts.js
@@ -6,14 +6,30 @@ $(document).ready(function() {
     const gender = parseInt($("#gender").val());
     const element = parseInt($("#element").val());
 
-    if (animal === color && color === food && food === gender && gender === element) {
+    if (animal === 1 && color === 1 && food === 1 && gender === 1 && element === 1) {
       $("#ruby1").show();
       $("#csharp").hide();
-    } else if (animal === 2 && color === animal) {
-      $("#csharp").show();
+      $("#jas1").hide();
+      $("#go").hide();
+      $("#python").hide();
+    } else if (animal === 2 && color === 2 & element === 3) {
       $("#ruby1").hide();
-    } else if (animal === 3 && color === animal)
-      $("jas1").show();
+      $("#csharp").show();
+      $("#jas1").hide();
+      $("#go").hide();
+      $("#python").hide();
+    } else if (animal === 3 && color === 3) {
+      $("#ruby1").hide();
+      $("#csharp").hide();
+      $("#jas1").show();
+      $("#go").hide();
+      $("#python").hide();
+    } else if (animal === 2 && color === 2 && food === 1 && element === 2)
+      $("#jas1").hide();
+      $("#ruby1").hide();
+      $("#csharp").hide();
+      $("#go").show();
+      $("#python").hide();
 
     event.preventDefault();
   });
